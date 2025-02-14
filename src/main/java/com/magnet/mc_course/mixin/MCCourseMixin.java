58d5c65f852b5f1645c339bdc147343c2e209fd5
magnetProgramming <magnet.magnetic448@passfwd.com>
2025-02-14 05:04:11 -0500

    Initial commit

---
+++ src/main/java/com/magnet/mc_course/mixin/MCCourseMixin.java
@@ -0,0 +1,13 @@
+package com.magnet.mc_course.mixin;
+
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
+public class MCCourseMixin
+{
+
+
+
+}
